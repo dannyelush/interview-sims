fe90f2a27a4c45e9e3ceb63de71869a7931923e8
Daniel Andoni <daniel.andoni@ness.com>
2025-03-25 11:50:39 +0200

    show generated interviews

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -122,3 +122,31 @@ export async function isAuthenticated() {
 
     return !!user;
 }
+
+export async function getInterviewsByUserId(userId: string): Promise<Interview[] | null> {
+    const interviews = await db.collection('interviews')
+        .where('userId', '==', userId)
+        .orderBy('createdAt', 'desc')
+        .get();
+
+    return interviews.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    })) as Interview[];
+}
+
+export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview[] | null> {
+    const {userId, limit = 20} = params;
+
+    const interviews = await db.collection('interviews')
+        .where('finalized', '==', 'true')
+        .where('userId', '!=', userId)
+        .orderBy('createdAt', 'desc')
+        .limit(limit)
+        .get();
+
+    return interviews.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    })) as Interview[];
+}
