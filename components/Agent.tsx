e77cfb15a061e699020f10fd76ad297493e5bcd3
Daniel Andoni <daniel.andoni@ness.com>
2025-03-25 12:44:17 +0200

    show feedback

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { interviewer } from '@/constants';
+import { createFeedback } from '@/lib/actions/general.action';
 import { cn } from '@/lib/utils';
 import { vapi } from '@/lib/vapi.sdk';
 import Image from 'next/image'
@@ -78,10 +79,11 @@ const Agent = ({userName, userId, type, interviewId, questions }: AgentProps) =>
     const handleGenerateFeedback = async (messages: SavedMessage[]) => {
         console.log('Generating feedback...');
 
-        const {success, id} = {
-            success: true,
-            id: '123'
-        }
+        const {success, feedbackId: id} = await createFeedback({
+            interviewId: interviewId!,
+            userId: userId!,
+            transcript: messages
+        });
 
         if(success && id) {
             router.push(`/interview/${interviewId}/feedback`);
